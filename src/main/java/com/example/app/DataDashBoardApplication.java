10fa28e72db6be482916c2c904218ce3b5170c1f
rajeshchiyaan <rajesh.java47@gmail.com>
2022-09-07 20:42:53 +0530

    Added sysout.

--- src/main/java/com/example/app/DataDashBoardApplication.java
+++ src/main/java/com/example/app/DataDashBoardApplication.java
@@ -10,6 +10,7 @@ public class DataDashBoardApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DataDashBoardApplication.class, args);
+		System.out.println("Hello World");
 	}
 
 }
