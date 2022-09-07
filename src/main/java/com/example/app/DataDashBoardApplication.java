9c74150b4581a74fef6fdde67a92b44c6d31d0a1
rajeshchiyaan <rajesh.java47@gmail.com>
2022-09-07 20:48:43 +0530

    Added calender feature.

--- src/main/java/com/example/app/DataDashBoardApplication.java
+++ src/main/java/com/example/app/DataDashBoardApplication.java
@@ -11,6 +11,7 @@ public class DataDashBoardApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DataDashBoardApplication.class, args);
 		System.out.println("Hello World");
+		System.out.println("Spring boot");
 	}
 
 }
